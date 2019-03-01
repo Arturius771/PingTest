f754ed1ef759afb20c250f4e3f87ac79924d31c6
Artur Foden <FODENA@tcd.ie>
2019-03-01 07:32:13 +0000

    tidying code before trying algorithm again

--- PingTest/PingIP.cs
+++ PingTest/PingIP.cs
@@ -20,20 +20,11 @@ namespace PingTest {
                 }
                 else {
                     string now = DateTime.Now.ToString("HH:mm");
-                    string latestLFT = "temp";
-                    Console.WriteLine("##" + now);
-                    Console.WriteLine("##" + latestLFT);
-                    failCount++;                    
-                    textBox.Text = count + ". Ping to " + pingBox.Text + " FAILED" + "\r\n" + textBox.Text;
+                    failCount++;
                     failureLabel.Text = failCount.ToString();
+                    textBox.Text = count + ". Ping to " + pingBox.Text + " FAILED" + "\r\n" + textBox.Text;                    
                     textBox.BackColor = Color.Salmon;
-                    if(latestLFT != now){
-                        Console.WriteLine("Fail added to list " + now);
-                        failureCount.Add(count + ". " + pingBox.Text + " FAILED " + DateTime.Now.ToString("HH:mm"));
-                    }
-                    else{                              
-                        Console.WriteLine("Fail NOT added to list");
-                    }
+                    failureCount.Add(count + ". " + pingBox.Text + " FAILED " + DateTime.Now.ToString("HH:mm"));
                 }
             }
             catch(Exception exception) {
